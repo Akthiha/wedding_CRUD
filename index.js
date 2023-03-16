997bc076970d19da53544b1f06f7f50b4852af08
Aung Khant Thiha <ak@Aungs-MacBook-Pro.local>
2023-03-17 09:57:21 +1100

    version 0.1

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 const express = require('express');
 const app = express();
-const ejs = require ('ejs')
-app.set ('view engine', 'ejs')
+const ejs = require('ejs');
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'your_postgres_user',
+  host: 'localhost',
+  database: 'rsvp',
+  password: 'your_postgres_password',
+  port: 5432,
+});
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
 const port = 3000;
 
+// Starting point
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-// const logger = require('./middlewares/logger');
-// const { createRsvp } = require('./controllers/rsvpController');
+app.get('/guestinfo', (req, res) => {
+  res.render('guestinfo');
+});
 
-// app.use(logger);
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.get('/rsvpForm', (req, res) => {
+  res.render('rsvpForm');
+});
 
-// app.post('/rsvp', createRsvp);
+app.post('/rsvp', (req, res) => {
+  const { name, email, attending, guests, main_dish_choice, table_number } = req.body;
+  const query = 'INSERT INTO wedding_rsvp (name, email, attending, guests, main_dish_choice, table_number) VALUES ($1, $2, $3, $4, $5, $6)';
+  const values = [name, email, attending, guests, main_dish_choice, table_number];
 
-//starting point
-app.get ("/", (req, res) =>{
-    res.render ("index");
-})
+  pool.query(query, values, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.send('Error Occured');
+    } else {
+      res.send('Thanks for your registration. Wait for email confirmation.');
+    }
+  });
+});
+
+app.get('/menu', (req, res) => {
+  res.render('menu');
+});
+
+app.get('/gallery', (req, res) => {
+  res.render('gallery');
+});
 
 app.listen(port, () => {
   console.log(`Server listening at ${port}`);
