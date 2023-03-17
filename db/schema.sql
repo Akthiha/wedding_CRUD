e45666f6a84a76ccb0aac2cb38e4af593d2bedcc
Aung Khant Thiha <ak@Aungs-MacBook-Pro.local>
2023-03-17 11:28:14 +1100

    presenting not good version

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE wedding_rsvp (
   email TEXT NOT NULL,
   attending BOOLEAN NOT NULL,
   guests INTEGER NOT NULL,
-  main_dish_choice TEXT,
+  main_dish_choice VARCHAR(20) CHECK (main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
   table_number INTEGER
 );
