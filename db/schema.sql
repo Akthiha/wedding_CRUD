404a2ccbaaaf2e2ec026a2112f030a6ee44755f8
Aung Khant Thiha <ak@Aungs-MacBook-Pro.local>
2023-03-19 18:19:17 +1100

    after login function added

--- db/schema.sql
+++ db/schema.sql
@@ -7,5 +7,31 @@ CREATE TABLE wedding_rsvp (
   attending BOOLEAN NOT NULL,
   guests INTEGER NOT NULL,
   main_dish_choice VARCHAR(20) CHECK (main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
-  table_number INTEGER
+  guest1_name TEXT,
+  guest1_main_dish_choice VARCHAR(20) CHECK (guest1_main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
+  guest2_name TEXT,
+  guest2_main_dish_choice VARCHAR(20) CHECK (guest2_main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
+  guest3_name TEXT,
+  guest3_main_dish_choice VARCHAR(20) CHECK (guest3_main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
+  guest4_name TEXT,
+  guest4_main_dish_choice VARCHAR(20) CHECK (guest4_main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
+  guest5_name TEXT,
+  guest5_main_dish_choice VARCHAR(20) CHECK (guest5_main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
+  guest6_name TEXT,
+  guest6_main_dish_choice VARCHAR(20) CHECK (guest6_main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
+  guest7_name TEXT,
+  guest7_main_dish_choice VARCHAR(20) CHECK (guest7_main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
+  guest8_name TEXT,
+  guest8_main_dish_choice VARCHAR(20) CHECK (guest8_main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
+  guest9_name TEXT,
+  guest9_main_dish_choice VARCHAR(20) CHECK (guest9_main_dish_choice IN ('chicken', 'fish', 'vegetarian')),
+  guest10_name TEXT,
+  guest10_main_dish_choice VARCHAR(20) CHECK (guest10_main_dish_choice IN ('chicken', 'fish', 'vegetarian'))
 );
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
--- index.js
+++ index.js
@@ -1,48 +1,243 @@
+// const express = require('express');
+// const app = express();
+// const ejs = require('ejs');
+// const { Pool } = require('pg');
+// const pool = new Pool(process.env.DATABASE_URL ? {
+//     connectionString: process.env.DATABASE_URL
+// } : { database: 'rsvp' });
+// app.set('view engine', 'ejs');
+// app.use(express.urlencoded({ extended: false }));
+
+// const port = process.env.PORT || 3000;
+
+// // Starting point
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+
+// app.get('/guestinfo', (req, res) => {
+//     res.render('guestinfo');
+// });
+
+// app.get('/rsvpForm', (req, res) => {
+//     res.render('rsvpForm');
+// });
+
+// // app.post('/rsvp', (req, res) => {
+// //     const { name, email, attending, guests, main_dish_choice, table_number } = req.body;
+// //     const query = 'INSERT INTO wedding_rsvp (name, email, attending, guests, main_dish_choice, table_number) VALUES ($1, $2, $3, $4, $5, $6)';
+// //     const values = [name, email, attending, guests, main_dish_choice, table_number];
+
+// //     pool.query(query, values, (err, result) => {
+// //         if (err) {
+// //             console.error(err);
+// //             res.send('Error Occured');
+// //         } else {
+// //             res.send('Thanks for your registration. Wait for email confirmation.');
+// //         }
+// //     });
+// // });
+
+// app.post('/rsvp', (req, res) => {
+//     const { name, email, attending, guests, main_dish_choice} = req.body;
+  
+//     let query = 'INSERT INTO wedding_rsvp (name, email, attending, guests, main_dish_choice)';
+//     let values = [name, email, attending, guests, main_dish_choice];
+  
+//     for (let i = 1; i <= guests; i++) {
+//       const guestName = req.body[`guest${i}_name`];
+//       const guestMainDishChoice = req.body[`guest${i}_main_dish_choice`];
+  
+//       if (guestName && guestMainDishChoice) {
+//         query += `, guest${i}_name, guest${i}_main_dish_choice`;
+//         values.push(guestName, guestMainDishChoice);
+//       }
+//     }
+  
+//     query += ' VALUES (';
+  
+//     for (let i = 1; i <= values.length; i++) {
+//       query += `$${i}`;
+//       if (i < values.length) {
+//         query += ', ';
+//       }
+//     }
+  
+//     query += ')';
+  
+//     pool.query(query,  values, (err, result) => {
+//           if (err) {
+//               console.error(err);
+//               res.send('Error Occured');
+//           } else {
+//               res.send('Thanks for your registration. Wait for email confirmation.');
+//           }
+//       });
+//   });
+  
+
+// app.get('/menu', (req, res) => {
+//     res.render('menu');
+// });
+
+// app.get('/gallery', (req, res) => {
+//     res.render('gallery');
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server listening at ${port}`);
+// });
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const { Pool } = require('pg');
+const session = require('express-session');
 const pool = new Pool(process.env.DATABASE_URL ? {
     connectionString: process.env.DATABASE_URL
 } : { database: 'rsvp' });
+const bcrypt = require('bcrypt');
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'my-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
 
 const port = process.env.PORT || 3000;
 
-// Starting point
 app.get('/', (req, res) => {
-    res.render('index');
+    const user = req.session.user;
+    res.render('index', { user }); 
 });
 
-app.get('/guestinfo', (req, res) => {
-    res.render('guestinfo');
+// Authentication 
+app.get('/signup', (req, res) => {
+    res.render('signup');
 });
 
-app.get('/rsvpForm', (req, res) => {
-    res.render('rsvpForm');
-});
-
-app.post('/rsvp', (req, res) => {
-    const { name, email, attending, guests, main_dish_choice, table_number } = req.body;
-    const query = 'INSERT INTO wedding_rsvp (name, email, attending, guests, main_dish_choice, table_number) VALUES ($1, $2, $3, $4, $5, $6)';
-    const values = [name, email, attending, guests, main_dish_choice, table_number];
-
-    pool.query(query, values, (err, result) => {
+app.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+    bcrypt.hash(password, 10, (err, hash) => {
         if (err) {
             console.error(err);
-            res.send('Error Occured');
+            res.render('signup', { error: 'An error occurred' });
         } else {
-            res.send('Thanks for your registration. Wait for email confirmation.');
+            pool.query('INSERT INTO users (username, password_digest) VALUES ($1, $2)', [username, hash], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    res.render('signup', { error: 'An error occurred' });
+                } else {
+                    res.redirect('/login');
+                }
+            });
         }
     });
 });
 
-app.get('/menu', (req, res) => {
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    pool.query('SELECT * FROM users WHERE username = $1', [username], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.render('login', { error: 'An error occurred' });
+        } else if (result.rows.length === 0) {
+            res.render('login', { error: 'Invalid username or password' });
+        } else {
+            const user = result.rows[0];
+            bcrypt.compare(password, user.password_digest, (err, match) => {
+                if (err) {
+                    console.error(err);
+                    res.render('login', { error: 'An error occurred' });
+                } else if (match) {
+                    req.session.user = user.username;
+                    res.redirect('/');
+                } else {
+                    res.render('login', { error: 'Invalid username or password' });
+                }
+            });
+        }
+    });
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            console.error(err);
+        }
+        res.redirect('/');
+    });
+});
+
+// Authentication 
+const requiresLogin = (req, res, next) => {
+    if (req.session && req.session.user) {
+        return next();
+    } else {
+        return res.redirect('/login');
+    }
+};
+
+// Routes that require authentication
+
+
+app.get('/guestinfo', requiresLogin, (req, res) => {
+    res.render('guestinfo');
+});
+
+app.get('/rsvpForm', requiresLogin, (req, res) => {
+    let guests = ''; //magic number
+    res.render('rsvpForm',{guests});
+});
+
+app.post('/rsvp', requiresLogin, (req, res) => {
+    const { name, email, attending, guests, main_dish_choice} = req.body;
+  
+    let query = 'INSERT INTO wedding_rsvp (name, email, attending, guests, main_dish_choice)';
+    let values = [name, email, attending, guests, main_dish_choice];
+  
+    for (let i = 1; i <= guests; i++) {
+      const guestName = req.body[`guest${i}_name`];
+      const guestMainDishChoice = req.body[`guest${i}_main_dish_choice`];
+  
+      if (guestName && guestMainDishChoice) {
+        query += `, guest${i}_name, guest${i}_main_dish_choice`;
+        values.push(guestName, guestMainDishChoice);
+      }
+    }
+  
+    query += ' VALUES (';
+  
+    for (let i = 1; i <= values.length; i++) {
+      query += `$${i}`;
+      if (i < values.length) {
+        query += ', ';
+      }
+    }
+  
+    query += ')';
+  
+    pool.query(query,  values, (err, result) => {
+          if (err) {
+              console.error(err);
+              res.send('Error Occured');
+          } else {
+              res.send('Thanks for your registration. Wait for email confirmation.');
+          }
+      });
+  });
+  
+
+app.get('/menu', requiresLogin, (req, res) => {
     res.render('menu');
 });
 
-app.get('/gallery', (req, res) => {
+app.get('/gallery', requiresLogin, (req, res) => {
     res.render('gallery');
 });
 
